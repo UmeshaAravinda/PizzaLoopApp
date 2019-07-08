0c63f0652766a248bc23e22d9272120cb3aa7787
UmeshaAravinda <aravindaumesha@gmail.com>
2019-07-08 10:27:05 +0530

    pizzaApp

---
+++ app/src/main/java/com/fct/customlist/BeginActivity.java
@@ -0,0 +1,26 @@
+package com.fct.customlist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class BeginActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin);
+        btn =(Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BeginActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/fct/customlist/PdetailsActivity.java
@@ -0,0 +1,13 @@
+package com.fct.customlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PdetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdetails);
+    }
+}
